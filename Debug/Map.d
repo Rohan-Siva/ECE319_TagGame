39062c32c0759e0befc97c89f82ecd6067b3c804
Rohan Siva <rohansiva123@gmail.com>
2025-04-18 00:37:58 -0500

    background + file formatting

--- Debug/Map.d
+++ Debug/Map.d
@@ -1,6 +1,8 @@
 # FIXED
 
-Map.o: ../Map.cpp ../Map.h ../../inc/ST7735.h ../Player.h
+Map.o: ../Map.cpp ../Map.h ../../inc/ST7735.h ../Player.h \
+ ../images/images.h
 ../Map.h:
 ../../inc/ST7735.h:
 ../Player.h:
+../images/images.h:
--- Map.h
+++ Map.h
@@ -7,13 +7,6 @@
 #define GRID_WIDTH 16
 #define GRID_HEIGHT 20
 
-extern const uint16_t WallSprite[64];     // 8x8 wall sprite
-extern const uint16_t CoinSprite[64];     // 8x8 coin sprite
-extern const uint16_t BorderSprite[64];   // 8x8 border sprite
-extern const uint16_t SpeedSprite[64];    // 8x8 speed powerup sprite
-extern const uint16_t ChaserSprite[64];   // 8x8 chaser enemy sprite
-extern const uint16_t RunnerSprite[64];   // 8x8 runner player sprite
-extern const uint16_t Black[64];          // 8x8 black tile sprite
 
 extern uint8_t map[GRID_HEIGHT][GRID_WIDTH];
 
