337b6e92913c5677c4076b5b7adf7bd06da467d3
Rohan Siva <rohansiva123@gmail.com>
2025-04-17 23:00:16 -0500

    header file additions

--- Map.h
+++ Map.h
@@ -7,17 +7,30 @@
 #define GRID_WIDTH 16
 #define GRID_HEIGHT 20
 
+extern const uint16_t WallSprite[64];     // 8x8 wall sprite
+extern const uint16_t CoinSprite[64];     // 8x8 coin sprite
+extern const uint16_t BorderSprite[64];   // 8x8 border sprite
+extern const uint16_t SpeedSprite[64];    // 8x8 speed powerup sprite
+extern const uint16_t ChaserSprite[64];   // 8x8 chaser enemy sprite
+extern const uint16_t RunnerSprite[64];   // 8x8 runner player sprite
+extern const uint16_t Black[64];          // 8x8 black tile sprite
+
 extern uint8_t map[GRID_HEIGHT][GRID_WIDTH];
 
-extern const uint16_t WallSprite[64];   // 8x8 wall sprite
-extern const uint16_t CoinSprite[64];   // 8x8 coin sprite
-extern const uint16_t BorderSprite[64];   // 8x8 coin sprite
-extern const uint16_t SpeedSprite[64];
+enum MenuSelection { MENU_PLAY, MENU_LANGUAGE, MENU_RULES };
+extern uint8_t currentSelection;
+extern uint8_t language;
+#define MENU_COUNT 3
+
+#define RULES_PAGE_COUNT 3
+extern uint8_t currentRulesPage;
 
 void DrawMap(void);
 void DrawMenu(void);
-void NextRulesPage(void);
+void NavigateMenu(int direction);
+void SelectMenuItem(void);
 void DrawRules(void);
+void NextRulesPage(void);
 void DrawScores(void);
 
 #endif // __MAP_H__
